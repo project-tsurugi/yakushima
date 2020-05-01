dcb3273f564c71de32c184b3cdd9039a9036c40c
thawk105 <thawk105@gmail.com>
2020-05-01 23:35:54 +0900

    declare child_length nad use it.

--- src/mt_interior_node.h
+++ src/mt_interior_node.h
@@ -6,17 +6,21 @@
 
 #include <cstdint>
 
+#include "mt_base_node.h"
 #include "mt_border_node.h"
 
 // forward declaration
-struct border_node;
+class border_node;
 
 namespace yakushima {
 class interior_node : private base_node {
+public:
+  static constexpr std::size_t child_length = 16;
+
 private:
   // first member of base_node is aligned along with cache line size.
   uint8_t nkeys_;
-  border_node *child[16]
+  border_node *child[child_length];
 };
 
 } // namespace yakushima
