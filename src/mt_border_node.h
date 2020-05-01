b9ded7c3e30dccbf94b95a25c0b27e20aa884b76
thawk105 <thawk105@gmail.com>
2020-05-01 23:34:59 +0900

    declare node_fanout and use it.

--- src/mt_border_node.h
+++ src/mt_border_node.h
@@ -14,12 +14,13 @@ struct interior_node;
 namespace yakushima {
 class border_node : private base_node {
 public:
+  static constexpr std::size_t node_fanout = 15;
 private:
   // first member of base_node is aligned along with cache line size.
   uint8_t nremoved_;
-  uint8_t keylen[15]_;
+  uint8_t keylen[node_fanout]_;
   uint64_t permutation_;
-  link_or_value lv[15]_;
+  link_or_value lv[node_fanout]_;
   border_node *next_;
   border_node *prev_;
   keysuffix_t keysuffixes_;
