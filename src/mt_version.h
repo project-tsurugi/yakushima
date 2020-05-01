75cf246f5909f390280118bdfc3768bf046601b3
thawk105 <thawk105@gmail.com>
2020-05-01 16:10:42 +0900

    change body of node_version64 to trivially copyable.

--- src/mt_version.h
+++ src/mt_version.h
@@ -15,31 +15,44 @@
 
 namespace yakushima {
 
+class node_version64_body {
+  bool locked: 1;
+  bool inserting: 1;
+  bool splitting: 1;
+  bool deleted: 1;
+  bool root: 1;
+  bool leaf: 1;
+  std::uint32_t vinsert: 16;
+  std::uint64_t vsplit: 41;
+  bool unused: 1;
+};
+
 class node_version64 {
 public:
-  void set(node_version64 newv) & {
-    storeRelease(&obj_, (newv.get_obj()));
+  void set(node_version64_body newv) &{
+    body_.store(newv, std::memory_order_release);
   }
 
-  std::uint64_t get_obj() const {
-    return loadAcquire(obj_);
+  /**
+   * @pre This function may be interrupted by other writer.
+   * So it must use atomic function.
+   * @return std::uint64_t
+   */
+  node_version64_body get_body() const {
+    return body_.load(std::memory_order_acquire);
+  }
+
+  /**
+   * @pre This function may be interrupted by other writer.
+   * So it must use atomic function.
+   * @return std::uint64_t
+   */
+  void get_locked() const {
+    // todo
   }
 
 private:
-  union {
-    std::uint64_t obj_;
-    struct {
-      bool locked: 1;
-      bool inserting: 1;
-      bool splitting: 1;
-      bool deleted: 1;
-      bool root: 1;
-      bool leaf: 1;
-      std::uint32_t vinsert: 16;
-      std::uint64_t vsplit: 41;
-      bool unused: 1;
-    };
-  };
+  std::atomic<node_version64_body> body_;
 };
 
 } // namespace yakushima
