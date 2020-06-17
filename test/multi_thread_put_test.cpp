db1304e1f4555993ef1c94412dbfe281f8f5c6c1
Takayuki Tanabe <thawk105@gmail.com>
2020-06-17 18:37:16 +0900

    use loop to rarely failing test.

--- test/multi_thread_put_test.cpp
+++ test/multi_thread_put_test.cpp
@@ -165,47 +165,51 @@ TEST_F(multi_thread_put_test, put_until_creating_interior_node) {
 }
 
 TEST_F(multi_thread_put_test, DISABLED_put_between_split_border_and_split_interior_with_no_shuffle) {
-  Token token;
-  ASSERT_EQ(masstree_kvs::enter(token), status::OK);
-  constexpr std::size_t ary_size = 100;
-  std::vector<std::tuple<std::string, std::string>> kv1;
-  std::vector<std::tuple<std::string, std::string>> kv2;
-  for (std::size_t i = 0; i < ary_size / 2; ++i) {
-    kv1.emplace_back(std::make_tuple(std::string(1, i), std::to_string(i)));
-  }
-  for (std::size_t i = ary_size / 2; i < ary_size; ++i) {
-    kv2.emplace_back(std::make_tuple(std::string(1, i), std::to_string(i)));
-  }
+  for (size_t i = 0; i < 100; ++i) {
+    masstree_kvs::init();
+    Token token;
+    ASSERT_EQ(masstree_kvs::enter(token), status::OK);
+    constexpr std::size_t ary_size = 100;
+    std::vector<std::tuple<std::string, std::string>> kv1;
+    std::vector<std::tuple<std::string, std::string>> kv2;
+    for (std::size_t i = 0; i < ary_size / 2; ++i) {
+      kv1.emplace_back(std::make_tuple(std::string(1, i), std::to_string(i)));
+    }
+    for (std::size_t i = ary_size / 2; i < ary_size; ++i) {
+      kv2.emplace_back(std::make_tuple(std::string(1, i), std::to_string(i)));
+    }
 
-  struct S {
-    static void work(std::vector<std::tuple<std::string, std::string>> &kv) {
-      for (auto &i : kv) {
-        std::string k(std::get<0>(i)), v(std::get<1>(i));
-        ASSERT_EQ(status::OK, masstree_kvs::put(std::string_view(k), v.data(), v.size()));
+    struct S {
+      static void work(std::vector<std::tuple<std::string, std::string>> &kv) {
+        for (auto &i : kv) {
+          std::string k(std::get<0>(i)), v(std::get<1>(i));
+          ASSERT_EQ(status::OK, masstree_kvs::put(std::string_view(k), v.data(), v.size()));
+        }
+      }
+    };
+
+    std::thread t(S::work, std::ref(kv2));
+    S::work(std::ref(kv1));
+    t.join();
+
+    std::vector<std::tuple<char *, std::size_t>> tuple_list;
+    constexpr std::size_t v_index = 0;
+    for (std::size_t i = 0; i < ary_size; ++i) {
+      std::string k(1, i);
+      masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
+                               tuple_list);
+      if (tuple_list.size() != i + 1) {
+        EXPECT_EQ(tuple_list.size(), i + 1);
+      }
+      ASSERT_EQ(tuple_list.size(), i + 1);
+      for (std::size_t j = 0; j < i + 1; ++j) {
+        std::string v(std::to_string(j));
+        ASSERT_EQ(memcmp(std::get<v_index>(tuple_list.at(j)), v.data(), v.size()), 0);
       }
     }
-  };
-
-  std::thread t(S::work, std::ref(kv2));
-  S::work(std::ref(kv1));
-  t.join();
-
-  std::vector<std::tuple<char *, std::size_t>> tuple_list;
-  constexpr std::size_t v_index = 0;
-  for (std::size_t i = 0; i < ary_size; ++i) {
-    std::string k(1, i);
-    masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
-                             tuple_list);
-    if (tuple_list.size() != i + 1) {
-      EXPECT_EQ(tuple_list.size(), i + 1);
-    }
-    ASSERT_EQ(tuple_list.size(), i + 1);
-    for (std::size_t j = 0; j < i + 1; ++j) {
-      std::string v(std::to_string(j));
-      ASSERT_EQ(memcmp(std::get<v_index>(tuple_list.at(j)), v.data(), v.size()), 0);
-    }
+    ASSERT_EQ(masstree_kvs::leave(token), status::OK);
+    masstree_kvs::fin();
   }
-  ASSERT_EQ(masstree_kvs::leave(token), status::OK);
 }
 
 TEST_F(multi_thread_put_test, DISABLED_put_between_split_border_and_split_interior_with_shuffle) {
