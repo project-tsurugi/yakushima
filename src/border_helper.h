ed63709fb07de0aeb8a7c547275e711984c06763
Takayuki Tanabe <thawk105@gmail.com>
2020-06-17 18:35:14 +0900

    delete useless codes.

--- src/border_helper.h
+++ src/border_helper.h
@@ -206,13 +206,8 @@ static void border_split(border_node *border,
   std::sort(shift_pos.begin(), shift_pos.end());
   std::size_t shifted_ctr(0);
   for (std::size_t &shift_po : shift_pos) {
-    if (shift_po + 1 - shifted_ctr != base_node::key_slice_length - 1) {
-      /**
-       * The if condition is a boundary check for shift (move) and is optimized to avoid tail shifting using L218,L219.
-       */
-      border->shift_left_base_member(shift_po + 1 - shifted_ctr, 1);
-      border->shift_left_border_member(shift_po + 1 - shifted_ctr, 1);
-    }
+    border->shift_left_base_member(shift_po + 1 - shifted_ctr, 1);
+    border->shift_left_border_member(shift_po + 1 - shifted_ctr, 1);
     ++shifted_ctr;
   }
   /**
