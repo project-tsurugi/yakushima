e4d055693a98679991798e8c2ba0513155ce5bae
Takayuki Tanabe <thawk105@gmail.com>
2020-06-19 17:53:55 +0900

    add many new tests.

--- test/README.md
+++ test/README.md
@@ -20,8 +20,11 @@
   2. delete repeatedly until vanishing only one border node.
   3. test of 2 which is using shuffled data.
   4. delete repeatedly until vanishing two border nodes which is across layer.
-  5. delete repeatedly until vanishing the structure which has one interior node having two border nodes.
-  6. delete repeatedly until vanishing the structure which has one interior node having two interior nodes.
+  5. test of 4 which is using shuffled data.
+  6. delete repeatedly until vanishing the structure which has one interior node having two border nodes.
+  7. test of 6 which is using shuffled data.
+  8. delete repeatedly until vanishing the structure which has one interior node having two interior nodes.
+  9. test of 8 which is using shuffled data.
 - scan_test.cpp
   1. scan against single put to one border.
   2. scan against multiple put same null char key whose length is different each other agaisnt single border node.
@@ -33,9 +36,15 @@
 - multi_thread_put_test.cpp<br>
 put by multi threads and verify final state by scan.
   1. multiple put same null char key whose length is different each other against single border node.
-  2. multiple put same null char key whose length is different each other against multiple border, which is across some layer.
-  3. multiple put repeatedly until first split of border node.
-  4. multiple put repeatedly until first split of interior node.
+  2. test of 1 which is using shuffled data.
+  3. multiple put same null char key whose length is different each other against multiple border, which is across some layer.
+  4. test of 3 which is using shuffled data.
+  5. multiple put repeatedly until first split of border node.
+  6. test of 5 which is using shuffled data.
+  7. multiple put repeatedly between first split of border node and first split of interior node.
+  8. test of 7 which is using shuffled data.
+  9. multiple put repeatedly until first split of interior node.
+  10. test of 9 which is using shuffled data.
   
 ## developer-test
 - base_node_test.cpp
--- test/multi_thread_put_test.cpp
+++ test/multi_thread_put_test.cpp
@@ -31,7 +31,49 @@ protected:
   }
 };
 
-TEST_F(multi_thread_put_test, put_same_null_char_key_slices_and_different_key_length_to_single_border_by_multi_thread) {
+TEST_F(multi_thread_put_test,
+       put_same_null_char_key_slices_and_different_key_length_to_single_border_by_multi_thread) {
+  Token token;
+  ASSERT_EQ(masstree_kvs::enter(token), status::OK);
+  constexpr std::size_t ary_size = 9;
+  std::vector<std::tuple<std::string, std::string>> kv1;
+  std::vector<std::tuple<std::string, std::string>> kv2;
+  for (std::size_t i = 0; i < 5; ++i) {
+    kv1.emplace_back(std::make_tuple(std::string(i, '\0'), std::to_string(i)));
+  }
+  for (std::size_t i = 5; i < ary_size; ++i) {
+    kv2.emplace_back(std::make_tuple(std::string(i, '\0'), std::to_string(i)));
+  }
+
+  struct S {
+    static void work(std::vector<std::tuple<std::string, std::string>> &kv) {
+      for (auto &i : kv) {
+        std::string k(std::get<0>(i)), v(std::get<1>(i));
+        ASSERT_EQ(status::OK, masstree_kvs::put(std::string_view(k), v.data(), v.size()));
+      }
+    }
+  };
+
+  std::thread t(S::work, std::ref(kv2));
+  S::work(std::ref(kv1));
+  t.join();
+
+  std::vector<std::tuple<char *, std::size_t>> tuple_list;
+  constexpr std::size_t v_index = 0;
+  for (std::size_t i = 0; i < ary_size; ++i) {
+    std::string k(i, '\0');
+    masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
+                             tuple_list);
+    for (std::size_t j = 0; j < i + 1; ++j) {
+      std::string v(std::to_string(j));
+      ASSERT_EQ(memcmp(std::get<v_index>(tuple_list.at(j)), v.data(), v.size()), 0);
+    }
+  }
+  ASSERT_EQ(masstree_kvs::leave(token), status::OK);
+}
+
+TEST_F(multi_thread_put_test,
+       put_same_null_char_key_slices_and_different_key_length_to_single_border_by_multi_thread_with_shuffle) {
   Token token;
   ASSERT_EQ(masstree_kvs::enter(token), status::OK);
   constexpr std::size_t ary_size = 9;
@@ -88,6 +130,47 @@ TEST_F(multi_thread_put_test,
   for (std::size_t i = 7; i < ary_size; ++i) {
     kv2.emplace_back(std::make_tuple(std::string(i, '\0'), std::to_string(i)));
   }
+
+  struct S {
+    static void work(std::vector<std::tuple<std::string, std::string>> &kv) {
+      for (auto &i : kv) {
+        std::string k(std::get<0>(i)), v(std::get<1>(i));
+        ASSERT_EQ(status::OK, masstree_kvs::put(std::string_view(k), v.data(), v.size()));
+      }
+    }
+  };
+
+  std::thread t(S::work, std::ref(kv2));
+  S::work(std::ref(kv1));
+  t.join();
+
+  std::vector<std::tuple<char *, std::size_t>> tuple_list;
+  constexpr std::size_t v_index = 0;
+  for (std::size_t i = 0; i < ary_size; ++i) {
+    std::string k(i, '\0');
+    masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
+                             tuple_list);
+    for (std::size_t j = 0; j < i + 1; ++j) {
+      std::string v(std::to_string(j));
+      ASSERT_EQ(memcmp(std::get<v_index>(tuple_list.at(j)), v.data(), v.size()), 0);
+    }
+  }
+  ASSERT_EQ(masstree_kvs::leave(token), status::OK);
+}
+
+TEST_F(multi_thread_put_test,
+       put_same_null_char_key_slices_and_different_key_length_to_multiple_border_by_multi_thread_with_shuffle) {
+  Token token;
+  ASSERT_EQ(masstree_kvs::enter(token), status::OK);
+  constexpr std::size_t ary_size = 15;
+  std::vector<std::tuple<std::string, std::string>> kv1;
+  std::vector<std::tuple<std::string, std::string>> kv2;
+  for (std::size_t i = 0; i < 7; ++i) {
+    kv1.emplace_back(std::make_tuple(std::string(i, '\0'), std::to_string(i)));
+  }
+  for (std::size_t i = 7; i < ary_size; ++i) {
+    kv2.emplace_back(std::make_tuple(std::string(i, '\0'), std::to_string(i)));
+  }
   std::random_device seed_gen;
   std::mt19937 engine(seed_gen());
   std::shuffle(kv1.begin(), kv1.end(), engine);
@@ -121,6 +204,46 @@ TEST_F(multi_thread_put_test,
 }
 
 TEST_F(multi_thread_put_test, put_until_creating_interior_node) {
+  Token token;
+  ASSERT_EQ(masstree_kvs::enter(token), status::OK);
+  constexpr std::size_t ary_size = base_node::key_slice_length + 1;
+  std::vector<std::tuple<std::string, std::string>> kv1;
+  std::vector<std::tuple<std::string, std::string>> kv2;
+  for (std::size_t i = 0; i < ary_size / 2; ++i) {
+    kv1.emplace_back(std::make_tuple(std::string(1, 'a' + i), std::to_string(i)));
+  }
+  for (std::size_t i = ary_size / 2; i < ary_size; ++i) {
+    kv2.emplace_back(std::make_tuple(std::string(1, 'a' + i), std::to_string(i)));
+  }
+
+  struct S {
+    static void work(std::vector<std::tuple<std::string, std::string>> &kv) {
+      for (auto &i : kv) {
+        std::string k(std::get<0>(i)), v(std::get<1>(i));
+        ASSERT_EQ(status::OK, masstree_kvs::put(std::string_view(k), v.data(), v.size()));
+      }
+    }
+  };
+
+  std::thread t(S::work, std::ref(kv2));
+  S::work(std::ref(kv1));
+  t.join();
+
+  std::vector<std::tuple<char *, std::size_t>> tuple_list;
+  constexpr std::size_t v_index = 0;
+  for (std::size_t i = 0; i < ary_size; ++i) {
+    std::string k(1, 'a' + i);
+    masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
+                             tuple_list);
+    for (std::size_t j = 0; j < i + 1; ++j) {
+      std::string v(std::to_string(j));
+      ASSERT_EQ(memcmp(std::get<v_index>(tuple_list.at(j)), v.data(), v.size()), 0);
+    }
+  }
+  ASSERT_EQ(masstree_kvs::leave(token), status::OK);
+}
+
+TEST_F(multi_thread_put_test, put_until_creating_interior_node_with_shuffle) {
   Token token;
   ASSERT_EQ(masstree_kvs::enter(token), status::OK);
   constexpr std::size_t ary_size = base_node::key_slice_length + 1;
