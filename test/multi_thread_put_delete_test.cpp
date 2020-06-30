cd7bb0b53dc033e3228cf995cb3bacec98732e22
Takayuki Tanabe <thawk105@gmail.com>
2020-06-30 17:03:21 +0900

    add new test.

--- test/multi_thread_put_delete_test.cpp
+++ test/multi_thread_put_delete_test.cpp
@@ -619,6 +619,93 @@ TEST_F(multi_thread_put_delete_test, test6) {
     masstree_kvs::fin();
   }
 }
+TEST_F(multi_thread_put_delete_test, test6_2) {
+  /**
+   * multiple layer version of test6
+   */
+
+  constexpr std::size_t ary_size = base_node::key_slice_length + 1;
+  std::vector<std::tuple<std::string, std::string>> kv1;
+  std::vector<std::tuple<std::string, std::string>> kv2;
+  for (std::size_t i = 0; i < ary_size / 2; ++i) {
+    kv1.emplace_back(std::make_tuple(std::string(8, UINT8_MAX) + std::string(1, i), std::to_string(i)));
+  }
+  for (std::size_t i = ary_size / 2; i < ary_size; ++i) {
+    kv2.emplace_back(std::make_tuple(std::string(8, UINT8_MAX) + std::string(1, i), std::to_string(i)));
+  }
+
+  std::random_device seed_gen;
+  std::mt19937 engine(seed_gen());
+
+#ifndef NDEBUG
+  for (std::size_t h = 0; h < 1; ++h) {
+#else
+    for (std::size_t h = 0; h < 100; ++h) {
+#endif
+    masstree_kvs::init();
+    Token token[2];
+    ASSERT_EQ(masstree_kvs::enter(token[0]), status::OK);
+    ASSERT_EQ(masstree_kvs::enter(token[1]), status::OK);
+
+    std::shuffle(kv1.begin(), kv1.end(), engine);
+    std::shuffle(kv2.begin(), kv2.end(), engine);
+
+    struct S {
+      static void work(std::vector<std::tuple<std::string, std::string>> &kv, Token &token) {
+        for (std::size_t j = 0; j < 10; ++j) {
+          for (auto &i : kv) {
+            std::string k(std::get<0>(i)), v(std::get<1>(i));
+            status ret = masstree_kvs::put(std::string_view(k), v.data(), v.size());
+            if (ret != status::OK) {
+              ASSERT_EQ(ret, status::OK);
+              std::abort();
+            }
+          }
+          for (auto &i : kv) {
+            std::string k(std::get<0>(i)), v(std::get<1>(i));
+            status ret = masstree_kvs::remove(token, std::string_view(k));
+            if (ret != status::OK) {
+              ASSERT_EQ(ret, status::OK);
+              std::abort();
+            }
+          }
+        }
+        for (auto &i : kv) {
+          std::string k(std::get<0>(i)), v(std::get<1>(i));
+          status ret = masstree_kvs::put(std::string_view(k), v.data(), v.size());
+          if (ret != status::OK) {
+            ASSERT_EQ(ret, status::OK);
+            std::abort();
+          }
+        }
+      }
+    };
+
+    std::thread t(S::work, std::ref(kv2), std::ref(token[0]));
+    S::work(std::ref(kv1), std::ref(token[1]));
+    t.join();
+
+    std::vector<std::tuple<char *, std::size_t>> tuple_list;
+    constexpr std::size_t v_index = 0;
+    for (std::size_t i = 1; i < ary_size; ++i) {
+      std::string k = std::string(8, UINT8_MAX) + std::string(1, i);
+      masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
+                               tuple_list);
+      if (tuple_list.size() != i + 1) {
+        masstree_kvs::scan<char>(std::string_view(0, 0), false, std::string_view(k), false,
+                                 tuple_list);
+        ASSERT_EQ(tuple_list.size(), i + 1);
+      }
+      for (std::size_t j = 0; j < i + 1; ++j) {
+        std::string v(std::to_string(j));
+        ASSERT_EQ(memcmp(std::get<v_index>(tuple_list.at(j)), v.data(), v.size()), 0);
+      }
+    }
+    ASSERT_EQ(masstree_kvs::leave(token[0]), status::OK);
+    ASSERT_EQ(masstree_kvs::leave(token[1]), status::OK);
+    masstree_kvs::fin();
+  }
+}
 
 TEST_F(multi_thread_put_delete_test, test7) {
   /**
