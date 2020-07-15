cbb2558e58c0a7b123ca9a1203aa0907d184a3a7
Takayuki Tanabe <thawk105@gmail.com>
2020-07-15 17:56:36 +0900

    reduce warnings.

--- bench/include/affinity.h
+++ bench/include/affinity.h
@@ -10,48 +10,29 @@
 #include <cstdint>
 #include <iostream>
 
-#define CPUID(INFO, LEAF, SUBLEAF) \
-  __cpuid_count(LEAF, SUBLEAF, INFO[0], INFO[1], INFO[2], INFO[3])
-
-#define GETCPU(CPU)                                                 \
-  {                                                                 \
-    uint32_t CPUInfo[4];                                            \
-    CPUID(CPUInfo, 1, 0);                                           \
-    /* CPUInfo[1] is EBX, bits 24-31 are APIC ID */                 \
-    if ((CPUInfo[3] & (1 << 9)) == 0) {                             \
-      CPU = -1; /* no APIC on chip */                               \
-    } else {                                                        \
-      CPU = (unsigned)CPUInfo[1] >> 24;                             \
-      /*unsigned int cores = ((unsigned)CPUInfo[1] >> 16) & 0xff;*/ \
-      /*if ((CPUInfo[3] & (1 << 28)) == 1) printf("HTT\n");*/       \
-      /*printf("total core number : %d\n", cores);*/                \
-    }                                                               \
-    if (CPU < 0) CPU = 0;                                           \
-  }
-
 #ifdef YAKUSHIMA_LINUX
+
 [[maybe_unused]] static void set_thread_affinity(const int myid) {
   using namespace std;
   static std::atomic<int> nprocessors(-1);
-  int local_nprocessors, desired;
-  local_nprocessors = nprocessors.load(memory_order_acquire);
+  int local_nprocessors{nprocessors.load(memory_order_acquire)};
+  int desired{};
   for (;;) {
     if (local_nprocessors != -1) {
       break;
-    } else {
-      desired = sysconf(_SC_NPROCESSORS_CONF);
-      if (nprocessors.compare_exchange_strong(
+    }
+    desired = sysconf(_SC_NPROCESSORS_CONF);
+    if (nprocessors.compare_exchange_strong(
             local_nprocessors, desired, memory_order_acq_rel, memory_order_acquire)) {
-        break;
-      }
+      break;
     }
   }
 
-  pid_t pid = syscall(SYS_gettid);
+  pid_t pid = syscall(SYS_gettid); // NOLINT
   cpu_set_t cpu_set;
 
   CPU_ZERO(&cpu_set);
-  CPU_SET(myid % local_nprocessors, &cpu_set);
+  CPU_SET(myid % local_nprocessors, &cpu_set); // NOLINT
 
   if (sched_setaffinity(pid, sizeof(cpu_set_t), &cpu_set) != 0) {
     std::cout << __FILE__ << " : " << __LINE__ << " : error" << std::endl;
--- bench/include/random.h
+++ bench/include/random.h
@@ -6,39 +6,41 @@
  * And Tanabe Takayuki custmized.
  */
 
-#include <stdint.h>
+#include <array>
+#include <cstdint>
 #include <random>
 
 #pragma once
 
 class Xoroshiro128Plus {
- public:
+public:
 
-  Xoroshiro128Plus () {
+  Xoroshiro128Plus() { // NOLINT
     std::random_device rnd;
-    s[0] = rnd();
-    s[1] = splitMix64(s[0]);
+    s.at(0) = rnd();
+    s.at(1) = splitMix64(s.at(0));
   }
 
-  uint64_t splitMix64(uint64_t seed) {
+  static uint64_t splitMix64(uint64_t seed) {
     uint64_t z = (seed += 0x9e3779b97f4a7c15);
-    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
-    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
-    return z ^ (z >> 31);
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9; // NOLINT
+    z = (z ^ (z >> 27)) * 0x94d049bb133111eb; // NOLINT
+    return z ^ (z >> 31); // NOLINT
   }
 
-  inline uint64_t rotl(const uint64_t x, int k) {
-    return (x << k) | (x >> (64 - k));
+  static inline uint64_t rotl(const uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k)); // NOLINT
   }
 
-  uint64_t next(void) {
-    const uint64_t s0 = s[0];
-    uint64_t s1 = s[1];
+  uint64_t next() {
+    const uint64_t s0 = s.at(0);
+    uint64_t s1 = s.at(1);
     const uint64_t result = s0 + s1;
 
     s1 ^= s0;
-    s[0] = rotl(s0, 24) ^ s1 ^ (s1 << 16);  // a, b
-    s[1] = rotl(s1, 37);                    // c
+    s.at(0) = rotl(s0, 24) ^ s1 ^ (s1 << 16);   // NOLINT
+    // a, b
+    s.at(1) = rotl(s1, 37);                    // c
 
     return result;
   }
@@ -49,22 +51,22 @@ class Xoroshiro128Plus {
      to 2^64 calls to next(); it can be used to generate 2^64
      non-overlapping subsequences for parallel computations. */
 
-  void jump(void) {
-    static const uint64_t JUMP[] = {0xdf900294d8f554a5, 0x170865df4b3201fc};
+  void jump() {
+    static const std::array<uint64_t, 2> JUMP{0xdf900294d8f554a5, 0x170865df4b3201fc};
 
     uint64_t s0 = 0;
     uint64_t s1 = 0;
-    for (uint64_t i = 0; i < sizeof JUMP / sizeof *JUMP; i++)
+    for (uint64_t i = 0; i < sizeof JUMP / sizeof JUMP.at(0); i++) { // NOLINT
       for (int b = 0; b < 64; b++) {
-        if (JUMP[i] & UINT64_C(1) << b) {
-          s0 ^= s[0];
-          s1 ^= s[1];
+        if (JUMP.at(i) & UINT64_C(1) << b) {
+          s0 ^= s.at(0);
+          s1 ^= s.at(1);
         }
         next();
       }
-
-    s[0] = s0;
-    s[1] = s1;
+    }
+    s.at(0) = s0;
+    s.at(1) = s1;
   }
 
   /* This is the long-jump function for the generator. It is equivalent to
@@ -72,25 +74,24 @@ class Xoroshiro128Plus {
      from each of which jump() will generate 2^32 non-overlapping
      subsequences for parallel distributed computations. */
 
-  void long_jump(void) {
-    static const uint64_t LONG_JUMP[] = {0xd2a98b26625eee7b,
-                                         0xdddf9b1090aa7ac1};
+  void long_jump() {
+    static const std::array<uint64_t, 2> LONG_JUMP{0xd2a98b26625eee7b, 0xdddf9b1090aa7ac1};
 
     uint64_t s0 = 0;
     uint64_t s1 = 0;
-    for (uint64_t i = 0; i < sizeof LONG_JUMP / sizeof *LONG_JUMP; i++)
+    for (uint64_t i = 0; i < sizeof LONG_JUMP / sizeof LONG_JUMP.at(0); i++) { // NOLINT
       for (int b = 0; b < 64; b++) {
-        if (LONG_JUMP[i] & UINT64_C(1) << b) {
-          s0 ^= s[0];
-          s1 ^= s[1];
+        if (LONG_JUMP.at(i) & UINT64_C(1) << b) {
+          s0 ^= s.at(0);
+          s1 ^= s.at(1);
         }
         next();
       }
-
-    s[0] = s0;
-    s[1] = s1;
+    }
+    s.at(0) = s0;
+    s.at(1) = s1;
   }
 
 private:
-  uint64_t s[2];
+  std::array<uint64_t, 2> s; // NOLINT
 };
