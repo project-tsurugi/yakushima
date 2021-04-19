e09925e16c2734e8f4fb5d7d907119b6d02c7596
Takayuki Tanabe <thawk105@gmail.com>
2021-04-19 12:12:43 +0900

    add multi storage test.

--- test/storage_test.cpp
+++ test/storage_test.cpp
@@ -2,6 +2,8 @@
  * @file storage_test.cpp
  */
 
+#include <cstring>
+
 #include "gtest/gtest.h"
 
 #include "kvs.h"
@@ -71,6 +73,41 @@ TEST_F(st, simple_list_storage) {// NOLINT
     ASSERT_EQ(status::OK, create_storage(st1));
     ASSERT_EQ(status::OK, list_storages(tuple));
     ASSERT_EQ(tuple.size(), 1);
+}
+
+TEST_F(st, simple_operate_some_storage) {// NOLINT
+    std::string st1{"st1"};
+    std::string st2{"st2"};
+    std::string k1{"k1"};
+    std::string k2{"k2"};
+    std::string k3{"k3"};
+    std::string k4{"k4"};
+    std::string k5{"k5"};
+    std::string k6{"k6"};
+    std::string v{"v"};
+    ASSERT_EQ(status::OK, create_storage(st1));
+    ASSERT_EQ(status::OK, create_storage(st2));
+    Token token{};
+    ASSERT_EQ(enter(token), status::OK);
+    ASSERT_EQ(put(st1, k1, v.data(), v.size()), status::OK);
+    ASSERT_EQ(put(st1, k2, v.data(), v.size()), status::OK);
+    ASSERT_EQ(put(st1, k3, v.data(), v.size()), status::OK);
+    ASSERT_EQ(put(st2, k4, v.data(), v.size()), status::OK);
+    ASSERT_EQ(put(st2, k5, v.data(), v.size()), status::OK);
+    ASSERT_EQ(put(st2, k6, v.data(), v.size()), status::OK);
+    std::vector<std::tuple<std::string, char*, std::size_t>> tuple_list;
+    constexpr std::size_t  key_index{0};
+    ASSERT_EQ(status::OK, scan(st1, "", scan_endpoint::INF, "", scan_endpoint::INF, tuple_list));
+    ASSERT_EQ(tuple_list.size(), 3);
+    ASSERT_EQ(memcmp(std::get<key_index>(tuple_list.at(0)).data(), k1.data(), k1.size()), 0);
+    ASSERT_EQ(memcmp(std::get<key_index>(tuple_list.at(1)).data(), k2.data(), k1.size()), 0);
+    ASSERT_EQ(memcmp(std::get<key_index>(tuple_list.at(2)).data(), k3.data(), k1.size()), 0);
+    ASSERT_EQ(status::OK, scan(st2, "", scan_endpoint::INF, "", scan_endpoint::INF, tuple_list));
+    ASSERT_EQ(tuple_list.size(), 3);
+    ASSERT_EQ(memcmp(std::get<key_index>(tuple_list.at(0)).data(), k4.data(), k1.size()), 0);
+    ASSERT_EQ(memcmp(std::get<key_index>(tuple_list.at(1)).data(), k5.data(), k1.size()), 0);
+    ASSERT_EQ(memcmp(std::get<key_index>(tuple_list.at(2)).data(), k6.data(), k1.size()), 0);
+    ASSERT_EQ(status::OK, leave(token));
 }
 
 }// namespace yakushima::testing
