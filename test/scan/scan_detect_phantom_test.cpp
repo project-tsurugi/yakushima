81c8f7a10dae1960aab7885637f6cb4a9d0f53a8
Takayuki Tanabe <thawk105@gmail.com>
2022-11-29 17:06:58 +0900

    add phantom test

--- test/scan/scan_detect_phantom_test.cpp
+++ test/scan/scan_detect_phantom_test.cpp
@@ -33,10 +33,11 @@ public:
     void TearDown() override { fin(); }
 
 private:
-    std::once_flag init_; // NOLINT
+    static inline std::once_flag init_; // NOLINT
 };
 
-TEST_F(scan_detect_phantom_test, scan_put_delete_check) { // NOLINT
+TEST_F(scan_detect_phantom_test,               // NOLINT
+       scan_put_delete_check_for_empty_tree) { // NOLINT
     Token s{};
     ASSERT_EQ(status::OK, enter(s));
     std::vector<std::tuple<std::string, char*, std::size_t>>
@@ -51,6 +52,35 @@ TEST_F(scan_detect_phantom_test, scan_put_delete_check) { // NOLINT
     auto nvp = nv.front();
 
     std::string v{"v"};
+    ASSERT_EQ(status::OK, put(s, st, "", v.data(), v.size()));
+    // check phantom
+    auto nv2 = nvp.second->get_stable_version();
+    ASSERT_NE(nvp.first, nv2);
+    ASSERT_EQ(status::OK, remove(s, st, ""));
+    // check phantom
+    auto nv3 = nvp.second->get_stable_version();
+    ASSERT_NE(nvp.first, nv3);
+    ASSERT_NE(nv2, nv3);
+    ASSERT_EQ(status::OK, leave(s));
+}
+
+TEST_F(scan_detect_phantom_test,                   // NOLINT
+       scan_put_delete_check_for_not_empty_tree) { // NOLINT
+    Token s{};
+    ASSERT_EQ(status::OK, enter(s));
+    std::string v{"v"};
+    ASSERT_EQ(status::OK, put(s, st, "a", v.data(), v.size()));
+    std::vector<std::tuple<std::string, char*, std::size_t>>
+            tup_lis{}; // NOLINT
+    std::vector<std::pair<node_version64_body, node_version64*>> nv;
+    ASSERT_EQ(status::OK, scan<char>(st, "", scan_endpoint::INF, "",
+                                     scan_endpoint::INF, tup_lis, &nv));
+    // check nv is not empty and size is one.
+    ASSERT_FALSE(nv.empty());
+    ASSERT_EQ(nv.size(), 1);
+    // get nvp from nv
+    auto nvp = nv.front();
+
     ASSERT_EQ(status::OK, put(s, st, "", v.data(), v.size()));
     // check phantom
     auto nv2 = nvp.second->get_stable_version();
