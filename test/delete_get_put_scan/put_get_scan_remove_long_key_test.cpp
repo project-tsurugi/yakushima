5a5682b075ec010953f99e0e5b6a606f14d35cd8
Takayuki Tanabe <thawk105@gmail.com>
2023-01-23 18:59:50 +0900

    fix test

--- test/delete_get_put_scan/put_get_scan_remove_long_key_test.cpp
+++ test/delete_get_put_scan/put_get_scan_remove_long_key_test.cpp
@@ -43,8 +43,9 @@ TEST_F(put_get_scan_remove_long_key_test, put_get) { // NOLINT
     ASSERT_EQ(status::OK, create_storage(st));
 
     std::string v{"v"};
-    for (std::size_t i = 1; i < 1024 * 100; i *= 2) {
-        LOG(INFO) << "test key size " << i << " bytes";
+    for (std::size_t i = 1024; i <= 1024 * 35; i += 1024) { // NOLINT
+        LOG(INFO) << "test key size " << i << " B";
+        LOG(INFO) << "test key size " << i / 1024 << " KB";
         std::string k(i, 'a');
         // test: put
         ASSERT_EQ(status::OK, put(s, st, k, v.data(), v.size()));
@@ -53,14 +54,30 @@ TEST_F(put_get_scan_remove_long_key_test, put_get) { // NOLINT
         std::pair<char*, std::size_t> tuple{};
         ASSERT_EQ(status::OK, get<char>(st, k, tuple));
 
-        // verify
+        // get verify
         std::string got(std::get<0>(tuple), std::get<1>(tuple));
         ASSERT_EQ(got, v);
 
-        // todo scan operator test
+        // test: scan
+        // when 36 KB, it cause stack-over-flow
+        std::vector<std::tuple<std::string, char*, std::size_t>>
+                tup_lis{}; // NOLINT
+        std::vector<std::pair<node_version64_body, node_version64*>> nv;
+        ASSERT_EQ(status::OK, scan<char>(st, "", scan_endpoint::INF, "",
+                                         scan_endpoint::INF, tup_lis, &nv));
 
-        // delete
+        // scan verify
+        ASSERT_EQ(tup_lis.size(), 1);
+        ASSERT_EQ(std::get<0>(tup_lis.front()), k);
+        got = std::string(std::get<1>(tup_lis.front()),
+                          std::get<2>(tup_lis.front()));
+        ASSERT_EQ(got, v);
+
+        // test: delete
         ASSERT_EQ(status::OK, remove(s, st, std::string_view(k)));
+
+        // delete verify
+        ASSERT_EQ(status::WARN_NOT_EXIST, get<char>(st, k, tuple));
     }
 
     // cleanup
