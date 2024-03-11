6d527656a0f667b345fadf8aaeacad5405db436f
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2024-03-11 23:46:23 +0900

    fix: add log level defintion and macro

--- include/log.h
+++ include/log.h
@@ -8,10 +8,35 @@ namespace yakushima {
 
 constexpr static std::string_view log_location_prefix = "/:yakushima ";
 
+/**
+ * @brief logging level constant for errors
+ */
+static constexpr std::int32_t log_error = 10;
+
+/**
+ * @brief logging level constant for warnings
+ */
+static constexpr std::int32_t log_warning = 20;
+
+/**
+ * @brief logging level constant for information
+ */
+static constexpr std::int32_t log_info = 30;
+
+/**
+ * @brief logging level constant for debug information
+ */
+static constexpr std::int32_t log_debug = 40;
+
+/**
+ * @brief logging level constant for traces
+ */
+static constexpr std::int32_t log_trace = 50;
+
 #define yakushima_log_entry                                                    \
-    VLOG(log_trace) << std::boolalpha << log_location_prefix << "-->"; // NOLINT
+    VLOG(log_trace) << std::boolalpha << log_location_prefix << "-->" // NOLINT
 
 #define yakushima_log_exit                                                     \
-    VLOG(log_trace) << std::boolalpha << log_location_prefix << "<--"; // NOLINT
+    VLOG(log_trace) << std::boolalpha << log_location_prefix << "<--" // NOLINT
 
 } // namespace yakushima
