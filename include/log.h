ceb171ec6f176771e5f2e6cac1da17cbffb7d87b
Takayuki Tanabe <tanabe@nautilus-technologies.com>
2024-03-11 17:25:27 +0900

    fix: improve log.h

--- include/log.h
+++ include/log.h
@@ -1,7 +1,17 @@
 #pragma once
 
+#include <iostream>
+
+#include "glog/logging.h"
+
 namespace yakushima {
 
 constexpr static std::string_view log_location_prefix = "/:yakushima ";
+
+#define yakushima_log_entry                                                    \
+    VLOG(log_trace) << std::boolalpha << log_location_prefix << "-->"; // NOLINT
+
+#define yakushima_log_exit                                                     \
+    VLOG(log_trace) << std::boolalpha << log_location_prefix << "<--"; // NOLINT
 
 } // namespace yakushima
