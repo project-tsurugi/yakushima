9590b7b8e8e3b2d1a49ec4fce50c9409add32a7f
Nobuhiro Ban <ban@nautilus-technologies.com>
2025-10-18 01:25:03 +0900

    add test for splitting root interior node

--- test/put/put_test.cpp
+++ test/put/put_test.cpp
@@ -79,6 +79,138 @@ TEST_F(put_test, put_to_root_border_split) { // NOLINT
     ASSERT_EQ(leave(token), status::OK);
 }
 
+TEST_F(put_test, put_to_root_interior_split_layer0) { // NOLINT
+    auto make_key = [](std::size_t i) {
+        std::ostringstream ss;
+        ss << std::hex << std::setw(6) << std::setfill('0') << i;
+        return ss.str();
+    };
+
+    tree_instance* ti{};
+    find_storage(st, &ti);
+    Token token{};
+    ASSERT_EQ(enter(token), status::OK);
+    constexpr std::size_t ary_size = 8 * (key_slice_length + 2);
+    std::array<std::string, ary_size> k{};
+    std::array<std::string, ary_size> v{};
+    for (std::size_t i = 0; i < ary_size; ++i) {
+        k.at(i).assign(make_key(i)); // NOLINT
+        v.at(i).assign(make_key(i)); // NOLINT
+    }
+    for (std::size_t i = 0; i < ary_size - 1; ++i) {
+        ASSERT_EQ(status::OK,
+                  put(token, st, k.at(i), v.at(i).data(), v.at(i).size()));
+    }
+
+    // check root is interior with 16 (key_slice_length+1) children
+    auto* n = ti->load_root_ptr();
+    ASSERT_FALSE(n->get_version_border());
+    auto* rn = dynamic_cast<interior_node*>(n);
+    ASSERT_EQ(rn->get_n_keys(), key_slice_length);
+    // children are all border node
+    for (std::size_t i = 0; i <= rn->get_n_keys(); ++i) {
+        ASSERT_TRUE(rn->get_child_at(i)->get_version_border()) << i;
+    }
+
+    // next put cause splitting
+    for (std::size_t i = ary_size - 1; i < ary_size; ++i) {
+        ASSERT_EQ(status::OK,
+                  put(token, st, k.at(i), v.at(i).data(), v.at(i).size()));
+    }
+
+    // check root is interior with 2 children
+    n = ti->load_root_ptr();
+    ASSERT_FALSE(n->get_version_border());
+    rn = dynamic_cast<interior_node*>(n);
+    ASSERT_EQ(rn->get_n_keys(), 1);
+    // check child 0 is interior node, not root
+    ASSERT_FALSE(rn->get_child_at(0)->get_version_border());
+    auto* in0 = dynamic_cast<interior_node*>(rn->get_child_at(0));
+    ASSERT_FALSE(in0->get_version_root());
+    // check child 1 is interior node, not root
+    ASSERT_FALSE(rn->get_child_at(1)->get_version_border());
+    auto* in1 = dynamic_cast<interior_node*>(rn->get_child_at(1));
+    ASSERT_FALSE(in1->get_version_root());
+    // sum of num children is key_slice_length+2
+    ASSERT_EQ(in0->get_n_keys() + in1->get_n_keys(), key_slice_length); // (key_slice_length+2-N0)-1 + (N0)-1
+
+    //display();
+
+    ASSERT_EQ(destroy(), status::OK_DESTROY_ALL);
+    ASSERT_EQ(leave(token), status::OK);
+}
+
+TEST_F(put_test, put_to_root_interior_split_layer1) { // NOLINT
+    auto make_key = [](std::size_t i) {
+        std::ostringstream ss;
+        ss << std::hex << std::setw(14) << std::setfill('0') << i;
+        return ss.str();
+    };
+
+    tree_instance* ti{};
+    find_storage(st, &ti);
+    Token token{};
+    ASSERT_EQ(enter(token), status::OK);
+    constexpr std::size_t ary_size = 8 * (key_slice_length + 2);
+    std::array<std::string, ary_size> k{};
+    std::array<std::string, ary_size> v{};
+    for (std::size_t i = 0; i < ary_size; ++i) {
+        k.at(i).assign(make_key(i)); // NOLINT
+        v.at(i).assign(make_key(i)); // NOLINT
+    }
+    for (std::size_t i = 0; i < ary_size - 1; ++i) {
+        ASSERT_EQ(status::OK,
+                  put(token, st, k.at(i), v.at(i).data(), v.at(i).size()));
+    }
+
+    // check root is border node with 1 child
+    auto* n = ti->load_root_ptr();
+    ASSERT_TRUE(n->get_version_border());
+    auto* bn = dynamic_cast<border_node*>(n);
+    ASSERT_EQ(bn->get_permutation_cnk(), 1);
+    // check L1 root is interior with 16 (key_slice_length+1) children
+    auto *l1r = bn->get_lv_at(0)->get_next_layer();
+    ASSERT_FALSE(l1r->get_version_border());
+    auto* rn = dynamic_cast<interior_node*>(l1r);
+    ASSERT_EQ(rn->get_n_keys(), key_slice_length);
+    // children are all border node
+    for (std::size_t i = 0; i <= rn->get_n_keys(); ++i) {
+        ASSERT_TRUE(rn->get_child_at(i)->get_version_border()) << i;
+    }
+
+    // next put cause splitting
+    for (std::size_t i = ary_size - 1; i < ary_size; ++i) {
+        ASSERT_EQ(status::OK,
+                  put(token, st, k.at(i), v.at(i).data(), v.at(i).size()));
+    }
+
+    // check root is border node with 1 child
+    n = ti->load_root_ptr();
+    ASSERT_TRUE(n->get_version_border());
+    bn = dynamic_cast<border_node*>(n);
+    ASSERT_EQ(bn->get_permutation_cnk(), 1);
+    // check L1 root is interior with 2 children
+    l1r = bn->get_lv_at(0)->get_next_layer();
+    ASSERT_FALSE(l1r->get_version_border());
+    rn = dynamic_cast<interior_node*>(l1r);
+    ASSERT_EQ(rn->get_n_keys(), 1);
+    // check child 0 is interior node, not root
+    ASSERT_FALSE(rn->get_child_at(0)->get_version_border());
+    auto* in0 = dynamic_cast<interior_node*>(rn->get_child_at(0));
+    ASSERT_FALSE(in0->get_version_root());
+    // check child 1 is interior node, not root
+    ASSERT_FALSE(rn->get_child_at(1)->get_version_border());
+    auto* in1 = dynamic_cast<interior_node*>(rn->get_child_at(1));
+    ASSERT_FALSE(in1->get_version_root());
+    // sum of num children is key_slice_length+2
+    ASSERT_EQ(in0->get_n_keys() + in1->get_n_keys(), key_slice_length); // (key_slice_length+2-N0)-1 + (N0)-1
+
+    //display();
+
+    ASSERT_EQ(destroy(), status::OK_DESTROY_ALL);
+    ASSERT_EQ(leave(token), status::OK);
+}
+
 TEST_F(put_test, one_key) { // NOLINT
     tree_instance* ti{};
     find_storage(st, &ti);
