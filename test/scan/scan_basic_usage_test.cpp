0e95d35923cd72b66f6f585ec6f09ea0d32c9946
Nobuhiro Ban <ban@nautilus-technologies.com>
2026-04-10 21:55:48 +0900

    test: add scan test of non-empty key and inf-endpoint

--- test/scan/scan_basic_usage_test.cpp
+++ test/scan/scan_basic_usage_test.cpp
@@ -8,6 +8,8 @@
 
 #include "kvs.h"
 
+#include "test_tool.h"
+
 using namespace yakushima;
 
 namespace yakushima::testing {
@@ -92,6 +94,49 @@ TEST_F(st, basic_usage) { // NOLINT
     ASSERT_EQ(true, verify());
     ASSERT_EQ(status::ERR_BAD_USAGE,
               scan<char>(test_storage_name, "", scan_endpoint::INF, "",
+                         scan_endpoint::EXCLUSIVE, tup_lis, &nv));
+    ASSERT_EQ(leave(token), status::OK);
+}
+
+TEST_F(st, inf_endpoint) {
+    // choose endpoint=inf case of basic, and change key not to empty
+    // key must be ignored, so same result
+    std::string k("k");
+    std::string v("v");
+    Token token{};
+    ASSERT_OK(enter(token));
+    ASSERT_OK(put(token, test_storage_name, k, v.data(), v.size()));
+    std::vector<std::tuple<std::string, char*, std::size_t>> tup_lis{};
+    std::vector<std::pair<node_version64_body, node_version64*>> nv;
+    auto verify = [&tup_lis, &nv, &v]() {
+        if (tup_lis.size() != 1) { return false; }
+        if (tup_lis.size() != nv.size()) { return false; }
+        if (std::get<2>(tup_lis.at(0)) != v.size()) { return false; }
+        if (memcmp(std::get<1>(tup_lis.at(0)), v.data(), v.size()) != 0) {
+            return false;
+        }
+        return true;
+    };
+    auto verify_no_exist = [&tup_lis, &nv]() {
+        if (!tup_lis.empty()) { return false; }
+        if (nv.size() != 1) { return false; }
+        return true;
+    };
+    // a < k < z
+    ASSERT_OK(scan<char>(test_storage_name, "z", scan_endpoint::INF,
+                         "", scan_endpoint::INF, tup_lis, &nv));
+    ASSERT_EQ(true, verify());
+    ASSERT_OK(scan<char>(test_storage_name, "z", scan_endpoint::INF, "",
+                         scan_endpoint::INCLUSIVE, tup_lis, &nv));
+    ASSERT_EQ(true, verify_no_exist());
+    ASSERT_OK(scan<char>(test_storage_name, "", scan_endpoint::INCLUSIVE, "a",
+                         scan_endpoint::INF, tup_lis, &nv));
+    ASSERT_EQ(true, verify());
+    ASSERT_OK(scan<char>(test_storage_name, "", scan_endpoint::EXCLUSIVE, "a",
+                         scan_endpoint::INF, tup_lis, &nv));
+    ASSERT_EQ(true, verify());
+    ASSERT_EQ(status::ERR_BAD_USAGE,
+              scan<char>(test_storage_name, "z", scan_endpoint::INF, "",
                          scan_endpoint::EXCLUSIVE, tup_lis, &nv));
     ASSERT_EQ(leave(token), status::OK);
 }
